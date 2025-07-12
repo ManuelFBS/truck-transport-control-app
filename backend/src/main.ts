b4a891307758c6f7d3cf3c870a6954897efed288
ManuelFBS <manuelf.borrego@gmail.com>
2025-07-12 12:29:59 -0400

    Modifc. en 'main.ts'...

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,24 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+        const app = await NestFactory.create(AppModule);
+        const config = new DocumentBuilder()
+                .setTitle('NestJS Clean Architecture API')
+                .setDescription('API for employee and user management')
+                .setVersion('1.0')
+                .addTag('users')
+                .addTag('employees')
+                .build();
+        const document = SwaggerModule.createDocument(app, config);
+
+        SwaggerModule.setup('api', app, document);
+
+        console.log('REDIS_HOST:', process.env.REDIS_HOST);
+        console.log('REDIS_PORT:', process.env.REDIS_PORT);
+
+        await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
