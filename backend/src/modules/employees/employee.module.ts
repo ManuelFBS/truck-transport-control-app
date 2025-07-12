008536e7c45181fac5ff7e67aee513bd39c8b63b
ManuelFBS <manuelf.borrego@gmail.com>
2025-07-12 11:19:15 -0400

    Creac. 'employee.module.ts'...

---
+++ backend/src/modules/employees/employee.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { EmployeeService } from '../../application/use-cases/employees/employee.service';
+import { EmployeeController } from '../../infrastructure/interface-adapters/controllers/employees/employee.controller';
+import { PrismaEmployeeRepository } from '../../infrastructure/repositories/employees/prisma-employee.repository';
+import { PrismaService } from '../../infrastructure/database/prisma/prisma.service';
+
+@Module({
+        controllers: [EmployeeController],
+        providers: [
+                EmployeeService,
+                PrismaService,
+                {
+                        provide: 'EmployeeRepository',
+                        useClass: PrismaEmployeeRepository,
+                },
+        ],
+})
+export class EmployeeModule {}
